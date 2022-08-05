0341d4eb94032f910acfb46d860d60ca4112bd46
hyun-woong <mr.woong0926@gmail.com>
2022-08-05 20:43:45 +0900

    signup export data, methods add

--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -1,23 +1,41 @@
 <template>
-  <form>
+  <!--.prevent 는 제출하고, 페이지를 다시 이동하지 않음-->
+  <form @submit.prevent="submitForm">
     <div>
       <label for="username">ID : </label>
-      <input id="username" type="text"/>
+      <input id="username" type="text" v-model="username"/>
     </div>
     <div>
       <label for="password">Password : </label>
-      <input id="password" type="text"/>
+      <input id="password" type="text" v-model="password"/>
     </div>
     <div>
       <label for="nickname">Nickname : </label>
-      <input id="nickname" type="text"/>
+      <input id="nickname" type="text" v-model="nickname"/>
     </div>
     <button type="submit">Login</button>
   </form>
 </template>
 
 <script>
-export default {};
+import {registerUser} from "@/api/index";
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      nickname: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      alert('성공!')
+      console.log('Form submit')
+      registerUser();
+    },
+  }
+};
 </script>
 
 <style></style>
