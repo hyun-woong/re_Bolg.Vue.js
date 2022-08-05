3d14c9033f8e39eff4e834bda5dc0443c9417b70
hyun-woong <mr.woong0926@gmail.com>
2022-08-05 22:14:23 +0900

    spring server add

--- BoardApi/src/main/java/com/example/boardapi/board/entity/Board.java
+++ BoardApi/src/main/java/com/example/boardapi/board/entity/Board.java
@@ -1,4 +1,24 @@
 package com.example.boardapi.board.entity;
 
+import com.example.boardapi.user.entity.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
 }
--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 function registerUser(userData) {
-    const url = 'http://localhost:3000/signup';
+    const url = 'http://localhost:8000/signup';
     return axios.post(url, userData);
 }
 
